6329d0186e7c3e375b5e55bf826e3c70e60e777b
Yong Wang <ywang712@gmail.com>
2016-06-01 05:27:08 +0000

    added purchase db

---
+++ app/views/purchases/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @purchase, :id, :name, :category, :quantity, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601042155) do
+ActiveRecord::Schema.define(version: 20160601051559) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20160601042155) do
     t.datetime "updated_at",  null: false
     t.integer  "employee_id"
     t.string   "status_type"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
